a76f3fcb564782f176c004577864b7f402e0fbfe
ceyah1499 <oscarliemena@gmail.com>
2023-01-07 18:15:12 +1100

    Some refactoring, added helper methods for draft
    ReadFromInstructionFile.java correlates to https://trello.com/c/SgDjrUuo/2-i-want-to-be-able-to-read-instructions-commands-to-be-performed-in-order-on-members-from-a-file-instructionfile
    
    Added skeletons for getMember() and deleteMember() methods in MembersDatabase class to help aid visualize the solution

--- ICT711Group/src/MembersDatabase.java
+++ ICT711Group/src/MembersDatabase.java
@@ -7,13 +7,27 @@ public class MembersDatabase {
 	public MembersDatabase() {
 		membersList = new ArrayList<Member>();
 	}
+	
 	public int getMember(String name, String mobile) {
-		return -1;
+		// if exists return index of element;
+		/* else */ return -1;
 	}
+	
 	public void updateMember(int index, Member member) {
 		// toDo updateMember
 	}
+	
 	public void addNewMember(Member member) {
 		membersList.add(member);
+	}
+	
+	public boolean deleteMember(String name, String mobile) {
+		/* check if exists */ 
+		int index = this.getMember(name, mobile);
+		if (index != -1) {
+			membersList.remove(index);
+			// if delete successful return true;
+		}
+		/* else */ return false;
 	}
 }
