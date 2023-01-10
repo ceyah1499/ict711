bf336cc0bcf82dcce35c9ef6341dd60cd3809b09
ceyah1499 <oscarliemena@gmail.com>
2023-01-11 09:37:29 +1100

    Implemented methods getMember(), updateMember(), and deleteMember()

--- ICT711Group/src/MembersDatabase.java
+++ ICT711Group/src/MembersDatabase.java
@@ -1,33 +1,51 @@
 import java.util.ArrayList;
 
-public class MembersDatabase {
-	
+public class MembersDatabase 
+{
 	private ArrayList<Member> membersList;
 	
-	public MembersDatabase() {
+	public MembersDatabase() 
+	{
 		membersList = new ArrayList<Member>();
 	}
 	
-	public int getMember(String name, String mobile) {
-		// if exists return index of element;
-		/* else */ return -1;
+	public int getMember(String name, String mobile) 
+	{
+		for (int i = 0; i < membersList.size(); i++) 
+		{
+			if (membersList.get(i).getName().equals(name) && membersList.get(i).getMobile().equals(mobile)) 
+			{
+				return i;
+			}
+		}
+		
+		return -1;
 	}
 	
-	public void updateMember(int index, Member member) {
-		// toDo updateMember
-	}
-	
-	public void addNewMember(Member member) {
+	public void addNewMember(Member member) 
+	{
 		membersList.add(member);
 	}
 	
-	public boolean deleteMember(String name, String mobile) {
-		/* check if exists */ 
+	public void updateMember(int index, Member member) 
+	{
+		membersList.get(index).setName(member.getName());
+		membersList.get(index).setBirthday(member.getBirthday());
+		membersList.get(index).setPassType(member.getPassType());
+		membersList.get(index).setMobile(member.getMobile());
+		membersList.get(index).setFee(member.getFee());
+	}
+	
+	public boolean deleteMember(String name, String mobile) 
+	{
 		int index = this.getMember(name, mobile);
-		if (index != -1) {
+		
+		if (index != -1) 
+		{
 			membersList.remove(index);
-			// if delete successful return true;
+			return true;
 		}
-		/* else */ return false;
+		
+		return false;
 	}
 }
