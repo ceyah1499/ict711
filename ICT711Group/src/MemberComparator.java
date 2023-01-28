609cb784e3134093d3424bcb02fd3b5f66b8d6f0
SergeyMazurov <mazurov.au@gmail.com>
2023-01-28 16:28:51 +1100

    Update MemberComparator.java
    Description MemberComparator

--- ICT711Group/src/MemberComparator.java
+++ ICT711Group/src/MemberComparator.java
@@ -1,6 +1,7 @@
-import java.util.Comparator;
-
+import java.util.Comparator; // for sorting objects
+// class MemberComparator implements an interface Comparator
 public abstract class MemberComparator implements Comparator<Member>
 {
+	// method for sorting
 	public abstract int compare(Member e1, Member e2);
 }
