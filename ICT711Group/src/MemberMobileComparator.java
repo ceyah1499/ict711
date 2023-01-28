1dea86aa5016c2c2c03f64b35bff69f3d2bc396f
SergeyMazurov <mazurov.au@gmail.com>
2023-01-28 16:45:24 +1100

    MemberNameComparator.java and MemberMobileComparator.java
    Description: MemberNameComparator and MemberMobileComparator

--- ICT711Group/src/MemberMobileComparator.java
+++ ICT711Group/src/MemberMobileComparator.java
@@ -1,6 +1,8 @@
+// class MemberMobileComparator is inherited from MemberComparator class
 public class MemberMobileComparator extends MemberComparator
 {
-	@Override
+	@Override // child class method is over-writing its base class method
+	// method "compare" compares members by their mobile number
 	public int compare(Member e1, Member e2)
 	{
 		return e1.getMobile().compareTo(e2.getMobile());
--- ICT711Group/src/MemberNameComparator.java
+++ ICT711Group/src/MemberNameComparator.java
@@ -1,6 +1,8 @@
+// class MemberNameComparator is inherited from MemberComparator class
 public class MemberNameComparator extends MemberComparator
 {
-	@Override
+	@Override // child class method is over-writing its base class method
+	// method "compare" compares members by name
 	public int compare(Member e1, Member e2)
 	{
 		return e1.getName().compareTo(e2.getName());
